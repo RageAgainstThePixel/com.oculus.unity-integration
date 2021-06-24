bd8d9be6b6537b9ae32681f38c8bb5f423663409
Stephen Hodgson <hodgson.designs@gmail.com>
2021-06-24 13:52:02 -0400

    fixed bone transform assignments

--- Oculus Unity-Integration/Packages/com.oculus.unity-integration/Runtime/VR/Scripts/Util/OVRSkeleton.cs
+++ Oculus Unity-Integration/Packages/com.oculus.unity-integration/Runtime/VR/Scripts/Util/OVRSkeleton.cs
@@ -199,8 +199,9 @@ public class OVRSkeleton : MonoBehaviour
             bone.Id = (OVRSkeleton.BoneId)_skeleton.Bones[i].Id;
             bone.ParentBoneIndex = _skeleton.Bones[i].ParentBoneIndex;
 
-            Transform trans = bone.Transform != null ? bone.Transform :
-                              bone.Transform = new GameObject(BoneLabelFromBoneId(_skeletonType, bone.Id)).transform;
+            Transform trans = bone.Transform != null
+                ? bone.Transform
+                : bone.Transform = new GameObject(BoneLabelFromBoneId(_skeletonType, bone.Id)).transform;
             trans.localPosition = flipX ? _skeleton.Bones[i].Pose.Position.FromFlippedXVector3f() : _skeleton.Bones[i].Pose.Position.FromFlippedZVector3f();
             trans.localRotation = flipX ? _skeleton.Bones[i].Pose.Orientation.FromFlippedXQuatf() : _skeleton.Bones[i].Pose.Orientation.FromFlippedZQuatf();
         }
@@ -242,7 +243,9 @@ public class OVRSkeleton : MonoBehaviour
             bindPoseBone.Id = bone.Id;
             bindPoseBone.ParentBoneIndex = bone.ParentBoneIndex;
 
-            Transform trans = bindPoseBone.Transform;
+            Transform trans = bindPoseBone.Transform != null
+                ? bindPoseBone.Transform
+                : bindPoseBone.Transform = new GameObject(BoneLabelFromBoneId(_skeletonType, bindPoseBone.Id)).transform;
             trans.localPosition = bone.Transform.localPosition;
             trans.localRotation = bone.Transform.localRotation;
         }
