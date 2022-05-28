7ee8d7d2567eb263a1fba41b5d70570e9a59d24c
Stephen Hodgson <hodgson.designs@gmail.com>
2022-05-28 14:40:13 -0700

    ensure audio plugin settings are correct
    revert InteractorDriver to ensure proper references for obsolete

--- Oculus Unity-Integration/Packages/com.oculus.unity-integration/Runtime/Interaction/Runtime/Scripts/Interaction/Core/InteractorDriver.cs
+++ Oculus Unity-Integration/Packages/com.oculus.unity-integration/Runtime/Interaction/Runtime/Scripts/Interaction/Core/InteractorDriver.cs
@@ -11,9 +11,10 @@ permissions and limitations under the License.
 ************************************************************************************/
 
 using System;
+using UnityEngine;
 
 namespace Oculus.Interaction.Deprecated
 {
     [Obsolete]
-    public class InteractorDriver { }
+    public class InteractorDriver : MonoBehaviour { }
 }
